55581d19883d7db2122d344f278f99bddf039eb9
sherlock <514160797@qq.com>
2024-05-19 17:26:00 +0800

    add reduce v2

--- profile_reduce.cpp
+++ profile_reduce.cpp
@@ -1,9 +1,11 @@
 extern int performCpuReduction();
 extern int performCudaReductionV0();
 extern int performCudaReductionV1();
+extern int performCudaReductionV2();
 
 int main(int args, char** argv) {
   // performCpuReduction();
   performCudaReductionV0();
   performCudaReductionV1();
+  performCudaReductionV2();
 }
