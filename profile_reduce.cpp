b9d505880f02736d76a024adf5b929f296443d0f
sherlock <514160797@qq.com>
2024-05-19 18:52:55 +0800

    added cuda_reduce_v6.cu file, implemented reduce6 kernel function, and added performCudaReductionV6 function that performs CUDA reduction using reduce6 kernel.

--- profile_reduce.cpp
+++ profile_reduce.cpp
@@ -1,15 +1,22 @@
+#include <stdio.h>
+
 extern int performCpuReduction();
 extern int performCudaReductionV0();
 extern int performCudaReductionV1();
 extern int performCudaReductionV2();
 extern int performCudaReductionV3();
 extern int performCudaReductionV4();
+extern int performCudaReductionV5();
+extern int performCudaReductionV6(const size_t);
 
 int main(int args, char** argv) {
+  size_t array_size = 1 << 28;
   // performCpuReduction();
-  performCudaReductionV0();
-  performCudaReductionV1();
-  performCudaReductionV2();
-  performCudaReductionV3();
-  performCudaReductionV4();
+  // performCudaReductionV0();
+  // performCudaReductionV1();
+  // performCudaReductionV2();
+  // performCudaReductionV3();
+  // performCudaReductionV4();
+  // performCudaReductionV5();
+performCudaReductionV6(array_size);
 }
