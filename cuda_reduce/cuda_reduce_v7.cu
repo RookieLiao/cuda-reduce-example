48e1f354f7295a3e0598315876aed412225f6412
sherlock <514160797@qq.com>
2024-06-03 10:03:49 +0800

    Added CUDA reduction version 7 implementation.

---
+++ cuda_reduce/cuda_reduce_v7.cu
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include "../utils.h"
+
+extern int cpuReduction(int* idata, size_t size);
+
+__inline__ __device__ int warpReduce(int localSum) {
+  localSum += __shfl_down_sync(0xFFFFFFFF, localSum, 16);
+  localSum += __shfl_down_sync(0xFFFFFFFF, localSum, 8);
+  localSum += __shfl_down_sync(0xFFFFFFFF, localSum, 4);
+  localSum += __shfl_down_sync(0xFFFFFFFF, localSum, 2);
+  localSum += __shfl_down_sync(0xFFFFFFFF, localSum, 1);
+  return localSum;
+}
+
+// multiple add
+template<size_t blockSize>
+__global__ void reduce7(int* idata_d, int* odata_d, size_t size) {
+  extern __shared__ int sdata[];
+  size_t tid = threadIdx.x;
+  size_t g_idx = (2 * blockSize) * blockIdx.x + tid;
+  size_t gridSize = (blockSize * 2) * gridDim.x;
+
+  sdata[tid] = 0;
+  while (g_idx < size) {
+    sdata[tid] += idata_d[g_idx] + idata_d[g_idx + blockSize];
+    g_idx += gridSize;
+  }
+  __syncthreads();
+
+  if (blockSize >= 512) {
+    if (tid < 256) { sdata[tid] += sdata[tid + 256]; }
+    __syncthreads();
+  }
+
+  if (blockSize >= 256) {
+    if (tid < 128) { sdata[tid] += sdata[tid + 128]; }
+    __syncthreads();
+  }
+
+  if (blockSize >= 128) {
+    if (tid < 64) { sdata[tid] += sdata[tid + 64]; }
+    __syncthreads();
+  }
+
+  // warp shuffle reduce
+  if (tid < 32) {
+    sdata[0] = warpReduce(sdata[tid]);
+    sdata[32] = warpReduce(sdata[tid + 32]);
+  }
+
+  if (tid == 0) { odata_d[blockIdx.x] = sdata[0] + sdata[32]; }
+}
+
+int performCudaReductionV7(const size_t elem_size) {
+  // set up device
+  int dev = 0;
+  cudaDeviceProp deviceProp;
+  cudaGetDeviceProperties(&deviceProp, 0);
+  printf("starting reduction at cuda_v7 device %d: %s\n", dev, deviceProp.name);
+
+  size_t bytes = elem_size * sizeof(int);
+
+  // allocate host memory
+  int* idata_h = (int*)malloc(bytes);
+  int* temp = (int*)malloc(bytes);
+
+  initialize_array(idata_h, elem_size);
+
+  double iStart, iElaps;
+
+  // cpu reduction
+  memcpy(temp, idata_h, bytes);
+
+  int cpu_sum = cpuReduction(temp, elem_size);
+
+  // cuda reduce
+  constexpr size_t block_size = 128;
+
+  dim3 block(block_size);
+  dim3 grid0(1024);
+
+  // allocate device memory
+  int* idata_d = NULL;
+  int* odata_d0 = NULL;
+
+  cudaMalloc((void**)(&idata_d), bytes);
+  cudaMalloc((void**)(&odata_d0), grid0.x * sizeof(int));
+
+  int* odata_h = (int*)malloc(grid0.x * sizeof(int));
+
+  cudaMemcpy(idata_d, idata_h, bytes, cudaMemcpyHostToDevice);
+  cudaDeviceSynchronize();
+  iStart = seconds();
+
+  size_t smem_size = block_size * sizeof(int);
+  reduce7<block_size><<<grid0, block, smem_size>>>(idata_d, odata_d0, elem_size);
+
+  cudaMemcpy(odata_h, odata_d0, grid0.x * sizeof(int), cudaMemcpyDeviceToHost);
+  int gpu_sum = cpuReduction(odata_h, grid0.x);
+  iElaps = seconds() - iStart;
+  float gpu_bw = bytes / iElaps / 1e9;
+  printf("reduction_v7 elapsed %lf ms, bandwidth %lf GB/s\n", iElaps * 1e3, gpu_bw);
+
+  free(idata_h);
+  free(temp);
+
+  cudaFree(idata_d);
+  cudaFree(odata_d0);
+
+  // check results
+  bool bResult = (gpu_sum == cpu_sum);
+  if (!bResult) printf("Test failed!\n");
+  return EXIT_SUCCESS;
+}
--- profile_reduce.cpp
+++ profile_reduce.cpp
@@ -8,16 +8,26 @@ extern int performCudaReductionV3(const size_t);
 extern int performCudaReductionV4(const size_t);
 extern int performCudaReductionV5(const size_t);
 extern int performCudaReductionV6(const size_t);
+extern int performCudaReductionV7(const size_t);
 
 int main(int args, char** argv) {
   size_t array_size = 1ULL << 28;
   printf("start profiling reduction with array size %zu    \n", array_size);
   performCpuReduction(array_size);
   performCudaReductionV0(array_size);
+  performCudaReductionV0(array_size);
+  performCudaReductionV1(array_size);
   performCudaReductionV1(array_size);
   performCudaReductionV2(array_size);
+  performCudaReductionV2(array_size);
+  performCudaReductionV3(array_size);
   performCudaReductionV3(array_size);
   performCudaReductionV4(array_size);
+  performCudaReductionV4(array_size);
+  performCudaReductionV5(array_size);
   performCudaReductionV5(array_size);
   performCudaReductionV6(array_size);
+  performCudaReductionV6(array_size);
+  performCudaReductionV7(array_size);
+  performCudaReductionV7(array_size);
 }
